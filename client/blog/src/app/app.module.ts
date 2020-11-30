54a87c170a1850ad7cbad2c7ffb9f6e7a268ba28
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2020-11-30 15:17:09 +0530

    Created required components
    Run:
    - ng g c home
    - ng g c about
    - ng g c register
    - ng g c login

--- client/blog/src/app/app.module.ts
+++ client/blog/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    HomeComponent,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
